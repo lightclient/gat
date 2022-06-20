3a9b445746f21ece554f1569c16d78676134f983
lightclient@protonmail.com <lightclient@protonmail.com>
2022-06-20 16:37:19 +0200

    fix typo and unlock account

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ gat = {}
 
 me = "0xc0ffee61108b46c8b84c63df14e3a607ac981e93"
 gat.me = me
-pk = "7b1020d5eb36d875178a6251eb77497b0ffdf639e7d1b8a7312dac15daf2bbe8"
+gat.pk = "7b1020d5eb36d875178a6251eb77497b0ffdf639e7d1b8a7312dac15daf2bbe8"
 
 found = false
 for (i = 0; i < personal.listAccounts.length; i++) {
@@ -16,6 +16,8 @@ if (!found) {
 	console.log("testing account not found, importing...")
 	personal.importRawKey(gat.pk, "")
 }
+
+personal.unlockAccount(me, "")
 
 function run_code (code, params) {
   if (params == null) { params = {} }
